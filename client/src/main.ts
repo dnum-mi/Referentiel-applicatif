5a17e6ea0cb2938bf34ad290be2a91657f4c9189
Thomas Bernard <thomashbernard@gmail.com>
2024-11-13 17:51:45 +0100

    feat: :lock: add krakend and keycloak oidc with authorization code flow

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,22 +1,18 @@
-import { VIcon } from '@gouvminint/vue-dsfr'
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import App from './App.vue'
+import { VIcon } from '@gouvminint/vue-dsfr';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import router from './router/index'
-import '@gouvfr/dsfr/dist/core/core.main.min.css'
+import router from './router/index';
+import '@gouvfr/dsfr/dist/core/core.main.min.css';
 
-import '@gouvfr/dsfr/dist/component/component.main.min.css'
-import '@gouvfr/dsfr/dist/utility/utility.main.min.css'
+import '@gouvfr/dsfr/dist/component/component.main.min.css';
+import '@gouvfr/dsfr/dist/utility/utility.main.min.css';
 
-import '@gouvminint/vue-dsfr/styles'
-import '@gouvfr/dsfr/dist/scheme/scheme.min.css'
+import '@gouvminint/vue-dsfr/styles';
+import '@gouvfr/dsfr/dist/scheme/scheme.min.css';
 
-import '@gouvfr/dsfr/dist/utility/icons/icons.min.css'
-import './main.css'
+import '@gouvfr/dsfr/dist/utility/icons/icons.min.css';
+import './main.css';
 
-createApp(App)
-  .use(createPinia())
-  .component('VICon', VIcon)
-  .use(router)
-  .mount('#app')
+createApp(App).use(createPinia()).component('VICon', VIcon).use(router).mount('#app');
