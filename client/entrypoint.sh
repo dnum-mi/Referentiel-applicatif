a90e09c62779659ec898915a43de8b1f5390bcdc
Thomas Bernard <thomashbernard@gmail.com>
2024-11-19 14:20:43 +0100

    ci: :construction_worker: add frontend build job in gitlab ci

---
+++ client/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DEFAULT_APP_ENV_PREFIX="VITE_RDA_"
+DEFAULT_APP_DIST_DIR="/code"
+
+if [ -z "$APP_ENV_PREFIX" ]; then
+    echo "APP_ENV_PREFIX is not set. Setting to default: $DEFAULT_APP_ENV_PREFIX"
+    export APP_ENV_PREFIX=$DEFAULT_APP_ENV_PREFIX
+fi
+if [ -z "$APP_DIST_DIR" ]; then
+    echo "APP_DIST_DIR is not set. Setting to default: $DEFAULT_APP_DIST_DIR"
+    export APP_DIST_DIR=$DEFAULT_APP_DIST_DIR
+fi
+
+echo "Prefix: $APP_ENV_PREFIX"
+echo "Directory: $APP_DIST_DIR"
+
+for i in $(env | grep "^$APP_ENV_PREFIX"); do
+    key=$(echo "$i" | cut -d '=' -f 1)
+    value=$(echo "$i" | cut -d '=' -f 2-)
+
+    echo "Setting $key=$value"
+
+    find "$APP_DIST_DIR" -type f -exec sed -i 's|'"${key}"'|'"${value}"'|g' {} \;  
+done
+
+# exec CMD from Dockerfile
+# exec "$@"
+
+# run nginx
+nginx -g 'daemon off;'
