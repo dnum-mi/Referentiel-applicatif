8945006a78430439b3e07fb0a0f4445e5b7261c6
Caroline Robillard <line.robillard@gmail.com>
2024-11-27 22:30:09 +0100

    refactor: :art: improve create application

--- server/src/application/application.controller.ts
+++ server/src/application/application.controller.ts
@@ -1,84 +1,63 @@
-import { Controller, Post, Body, Patch, Param, Request, Logger, Get, Query, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Request,
+  Logger,
+  Get,
+  Query,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { UserService } from '../user/user.service';
-import { CreateApplicationDto, } from './dto/create-application.dto';
+import { CreateApplicationDto } from './dto/create-application.dto';
 import { UpdateApplicationDto } from './dto/update-application.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { SearchApplicationDto } from './dto/search-application.dto';
 import { GetApplicationDto } from './dto/get-application.dto';
-import * as jose from 'jose'
+import * as jose from 'jose';
 
 @ApiTags('applications')
 @Controller('applications')
 export class ApplicationController {
   applicationsService: ApplicationService;
+
   constructor(
     private readonly applicationService: ApplicationService,
-    private readonly userService: UserService
+    private readonly userService: UserService,
   ) {}
 
   @Post()
   @ApiOperation({ summary: 'Créer une nouvelle application' })
   @ApiResponse({ status: 201, description: 'Application créée avec succès.' })
   @ApiResponse({ status: 404, description: 'Metadata ou parent non trouvé.' })
-  async create(
+  public async create(
     @Body() createApplicationDto: CreateApplicationDto,
-    @Request() req
+    @Request() req,
   ) {
     Logger.log('create method called');
 
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) {
-      throw new UnauthorizedException('En-tête Authorization manquant');
-    }
+    const decodedToken = this.getDecodedToken(req);
+    const userFromDb = await this.findOrCreateUser(decodedToken);
 
-    const token = authHeader.split(' ')[1];
-    if (!token) {
-      throw new UnauthorizedException('Token Bearer non fourni');
-    }
+    const newApplication = await this.applicationService.createApplication(
+      userFromDb.keycloakId,
+      createApplicationDto,
+    );
 
-    let decodedToken;
-    try {
-      decodedToken = jose.decodeJwt(token);
-    } catch (error) {
-      Logger.error('Erreur lors du décodage du JWT :', error);
-      throw new UnauthorizedException('JWT invalide ou malformé');
-    }
-
-
-    if (!decodedToken.email || !decodedToken.sub) {
-      throw new UnauthorizedException('JWT manquant de claims requis (email ou sub)');
-    }
-
-    try {
-      const userFromDb = await this.userService.findOrCreateByEmail(
-        <string>decodedToken.email,
-        <string>decodedToken.sub
-      );
-
-      if (!userFromDb) {
-        throw new UnauthorizedException('Utilisateur non trouvé');
-      }
-
-      const newApplication = await this.applicationService.createApplication(
-        userFromDb.keycloakId,
-        createApplicationDto
-      );
-
-      return {
-        status: 201,
-        message: 'Application créée avec succès',
-        data: newApplication,
-      };
-    } catch (error) {
-      Logger.error('Erreur lors de la création de l\'application :', error);
-      throw new UnauthorizedException('Échec de la création de l\'application');
-    }
+    return {
+      status: 201,
+      message: 'Application créée avec succès',
+      data: newApplication,
+    };
   }
 
   @Get('search')
   async searchApplications(@Query() searchParams: SearchApplicationDto) {
-    console.log(searchParams); 
+    console.log(searchParams);
     return this.applicationService.searchApplications(searchParams);
   }
 
@@ -91,7 +70,6 @@ export class ApplicationController {
       throw new NotFoundException(error);
     }
   }
-
 
   @Patch(':id')
   @ApiOperation({ summary: 'Mettre à jour une application existante' })
@@ -106,4 +84,45 @@ export class ApplicationController {
   }
 
 
+  private getDecodedToken(req: any): any {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      throw new UnauthorizedException('En-tête Authorization manquant');
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Token Bearer non fourni');
+    }
+
+    try {
+      return jose.decodeJwt(token);
+    } catch (error) {
+      Logger.error('Erreur lors du décodage du JWT :', error);
+      throw new UnauthorizedException('JWT invalide ou malformé');
+    }
+  }
+
+
+  private async findOrCreateUser(decodedToken: any) {
+    if (!decodedToken.email || !decodedToken.sub) {
+      throw new UnauthorizedException('JWT manquant de claims requis (email ou sub)');
+    }
+
+    try {
+      const user = await this.userService.findOrCreateByEmail(
+        <string>decodedToken.email,
+        <string>decodedToken.sub,
+      );
+
+      if (!user) {
+        throw new UnauthorizedException('Utilisateur non trouvé');
+      }
+
+      return user;
+    } catch (error) {
+      Logger.error('Erreur lors de la récupération/création de l\'utilisateur :', error);
+      throw new UnauthorizedException('Échec lors de la gestion de l\'utilisateur');
+    }
+  }
 }
