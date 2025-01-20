1f3d8e94589cd196b4016e7ca5b54be79d082a7d
Caroline Robillard <45307611+Carolinedanslesnuages@users.noreply.github.com>
2025-01-20 10:38:40 +0100

    fix: :technologist: Delete token in log and add pretty json for dev (#241)

---
+++ backend/src/logger/logger.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
+
+@Injectable()
+export class LoggerService {
+  constructor(private readonly logger: PinoLogger) {}
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string, trace?: string) {
+    this.logger.error({ trace }, message);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+
+  verbose(message: string) {
+    this.logger.trace(message);
+  }
+}
