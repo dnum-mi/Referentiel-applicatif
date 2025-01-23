6d7a67ad047eb4fd520f6afe53ec9d87d2695248
Thomas Bernard <thomashbernard@gmail.com>
2025-01-23 17:19:16 +0100

    test: :white_check_mark: add test for application create

--- backend/tests/applications.e2e-spec.ts
+++ backend/tests/applications.e2e-spec.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
+import { v4 as uuidv4 } from 'uuid';
 import { setupTestSuite } from './setup';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('Applications', () => {
   const getApp = setupTestSuite();
@@ -12,5 +14,47 @@ describe('Applications', () => {
   it(`/GET applications/search`, () => {
     const app = getApp();
     return request(app.getHttpServer()).get('/applications/search').expect(200);
+  });
+
+  it(`/POST applications`, async () => {
+    const app = getApp();
+    const keycloakId = uuidv4();
+    const prismaService = new PrismaService();
+    await prismaService.user.create({
+      data: {
+        email: `${keycloakId}@test.fr`,
+        keycloakId: keycloakId,
+      },
+    });
+
+    return request(app.getHttpServer())
+      .post('/applications')
+      .send({
+        label: 'My Complete Application',
+        shortName: 'complete-app',
+        description:
+          'A comprehensive application example with all data filled.',
+        purposes: ['finance', 'HR', 'operations'],
+        tags: ['tag1', 'tag2', 'tag3'],
+        parentId: null,
+        lifecycle: {
+          status: 'in_production',
+          firstProductionDate: '2025-01-06T10:34:25.061Z',
+          plannedDecommissioningDate: '2030-12-31T23:59:59.000Z',
+        },
+        actors: [
+          {
+            role: 'dev',
+            userId: keycloakId,
+          },
+        ],
+        compliances: [],
+        externals: [],
+      })
+      .set(
+        'Authorization',
+        'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwYXBzYjYxQzVFa2x6d2JjRUpXYXZPeXllMk5UQ29FRHpRb2lFdWFlTjJnIn0.eyJzdWIiOiI5OWZhNDE5ZS1mNGZiLTRlZDctOGIxMS1jNzIxMGIzMDkiLCAiZW1haWwiOiJ0aG9tYXMuYmVybmFyZC1lY29ub2NvbUBpbnRlcmlldXIuZ291di5mciJ9.',
+      )
+      .expect(201);
   });
 });
