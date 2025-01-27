b08c73ba8ec254aab7d1a108241e8f43cdf84551
Thomas Bernard <thomashbernard@gmail.com>
2025-01-27 15:21:38 +0100

    refactor: :recycle: refactor auth middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -2,44 +2,23 @@ import {
   Injectable,
   NestMiddleware,
   UnauthorizedException,
-  Logger,
 } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { AuthUtils } from '../utils/helpers';
 import { UserService } from '../user/user.service';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(
-    private userService: UserService,
-    private prisma: PrismaService,
-  ) {}
+  constructor(private userService: UserService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
     try {
       const decodedToken = AuthUtils.getDecodedToken(req);
-      const userFromDb = await AuthUtils.findOrCreateUser(
+      req.user = await AuthUtils.findOrCreateUser(
         decodedToken,
         this.userService,
       );
-
-      const userExists = await this.prisma.user.findUnique({
-        where: { keycloakId: userFromDb.keycloakId },
-      });
-
-      if (userExists) {
-        req.user = userFromDb;
-        next();
-
-        Logger.log({
-          message: 'Passage dans le middleware',
-          userId: userFromDb.sub,
-          action: 'AuthMiddleware',
-        });
-      } else {
-        throw new UnauthorizedException("L'utilisateur n'existe pas");
-      }
+      next();
     } catch (error) {
       throw new UnauthorizedException("L'authentification a échoué");
     }
